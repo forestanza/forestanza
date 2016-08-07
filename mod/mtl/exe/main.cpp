74b13d58b1cd4d4e15cc4901939dae0828ac6e9f
Amerlyq <amerlyq@gmail.com>
2016-08-08 00:55:21 +0300

    [mtl] port main to curlcpp

--- mod/mtl/exe/main.cpp
+++ mod/mtl/exe/main.cpp
@@ -1,12 +1,32 @@
-#include <cpr/cpr.h>
 #include <json.hpp>
 
+#include <curl_easy.h>
+#include <curl_form.h>
+#include <curl_ios.h>
+#include <curl_exception.h>
+
 #include <iostream>
+#include <ostream>
 
 int main(int /* argc */, char** /* argv */)
 {
-    auto response = cpr::Get(cpr::Url{"https://httpbin.org/get"});
-    auto json = nlohmann::json::parse(response.text);
+    std::ostringstream oss;
+    curl::curl_ios<std::ostringstream> writer(oss);
+    curl::curl_easy req(writer);
+
+    req.add<CURLOPT_URL>("https://httpbin.org/get");
+    req.add<CURLOPT_FOLLOWLOCATION>(1L);
+    req.add<CURLOPT_SSL_VERIFYPEER>(0L);
+
+    try {
+        req.perform();
+    } catch (curl::curl_easy_exception error) {
+        // NOTE: printing the stack will erase it
+        error.print_traceback();
+        // ALT: curl::curlcpp_traceback errors = error.get_traceback();
+    }
+
+    auto json = nlohmann::json::parse(oss.str());
     std::cout << json.dump(2) << std::endl;
     std::cout << "done" << std::endl;
     return 0;
